5a3469d3577e7bd0ddafb9b024e8ca4fceb1841d
Spoke <spoke@Spokede-MacBook-Air.local>
2019-01-05 19:53:33 +0800

    test giphy api using NSURLSession

--- GifViewer/CollectionViewController.m
+++ GifViewer/CollectionViewController.m
@@ -20,25 +20,18 @@ static NSString * const reuseIdentifier = @"GifViewerCell";
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations
-//     self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Register cell classes
-//    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
-    
-    // Do any additional setup after loading the view.
-    
+    [self refershImages];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void) refershImages {
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURL *url = [NSURL URLWithString:@"https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&rating=pg"];
+    NSURLSessionDownloadTask *task = [session downloadTaskWithURL:(url) completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSString *responseText = [[NSString alloc] initWithContentsOfURL:location encoding:NSUTF8StringEncoding error:nil];
+        NSLog(@"Response: %@", responseText);
+    }];
+    [task resume];
 }
-*/
 
 #pragma mark <UICollectionViewDataSource>
 
